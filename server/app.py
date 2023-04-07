850cf8f15c216375e0e1e9caa3b12a0d24204e9b
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-06 22:06:02 -0400

    patch and delete complete

--- server/app.py
+++ server/app.py
@@ -44,7 +44,46 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        if plant == None:
+            response = make_response(
+                {"message": "No plant found!"},
+                404
+            )
+            return response
+        
+        data = request.get_json()
+        
+        for attr in data:
+            setattr(plant, attr, data[attr])
 
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(
+            jsonify(plant_dict),
+            200,
+            {"Content-Type": "application/json"},
+        )
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response(
+            {"message": "No content"},
+            204,
+        )
+        return response
+    
 api.add_resource(PlantByID, '/plants/<int:id>')
         
 
